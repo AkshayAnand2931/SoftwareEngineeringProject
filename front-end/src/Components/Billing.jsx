9471819c3a918120ed05af8440f9271f64fb3bf7
AadithyaRao2809 <aadithyarao@gmail.com>
2023-11-17 15:05:27 +0530

    add basic ui for billing and transaction

--- front-end/src/Components/Billing.jsx
+++ front-end/src/Components/Billing.jsx
@@ -1,10 +1,123 @@
-import React from 'react';
+// BillingPage.js
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Table, Form, Button } from 'react-bootstrap';
 
-function Billing(){
+const BillingPage = () => {
+  const [items, setItems] = useState([
+  ]);
 
-    return(
-        <h1>Billing</h1>
-    );
-}
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [formData, setFormData] = useState({ name: '', price: '' });
 
-export default Billing;
+  const addItemToBilling = () => {
+    const newItem = {
+      id: items.length + 1,
+      name: formData.name,
+      price: parseFloat(formData.price),
+    };
+    setItems([...items, newItem]);
+    console.log(items);
+    setFormData({ name: '', price: '' });
+  };
+
+  const removeItemFromBilling = (itemId) => {
+    const updatedItems = selectedItems.filter((item) => item.id !== itemId);
+    setSelectedItems(updatedItems);
+  };
+
+  const calculateTotalAmount = () => {
+    return selectedItems.reduce((total, item) => total + item.price, 0);
+  };
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    addItemToBilling();
+  };
+
+  const handleProceedToCheckout = () => {
+    // Implement the checkout logic here
+    console.log('Proceeding to checkout:', items);
+  };
+
+  return (
+    <Container className="mt-4">
+      <Row>
+        <Col md={4}>
+          <Card>
+            <Card.Header>
+              <h4>Add New Item</h4>
+            </Card.Header>
+            <Card.Body>
+              <Form onSubmit={handleFormSubmit}>
+                <Form.Group controlId="itemName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter item name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleFormChange}
+                  />
+                </Form.Group>
+                <Form.Group controlId="itemPrice">
+                  <Form.Label>Price</Form.Label>
+                  <Form.Control
+                    type="number"
+                    placeholder="Enter item price"
+                    name="price"
+                    value={formData.price}
+                    onChange={handleFormChange}
+                  />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Add Item
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={8}>
+          <Card>
+            <Card.Header>
+              <h4>Current Bill</h4>
+            </Card.Header>
+            <Card.Body>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {items.map((item) => (
+                    <tr key={item.id}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>${item.price}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col className="text-right">
+          <Button variant="success" onClick={handleProceedToCheckout}>
+            Proceed to Checkout
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default BillingPage;
--- front-end/src/Components/Transaction.jsx
+++ front-end/src/Components/Transaction.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+// TransactionHistory.js
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Table } from 'react-bootstrap';
 
-function Transaction(){
+const TransactionHistory = () => {
+  const [transactions, setTransactions] = useState([
+    { id: 1, date: '2023-01-01', totalAmount: 30, employeeId: 'E001' },
+    { id: 2, date: '2023-02-15', totalAmount: 25, employeeId: 'E002' },
+    // Add more transactions as needed
+  ]);
 
-    return(
-        <h1>Transaction</h1>
-    );
-}
+  return (
+    <Container className="mt-4">
+      <Row>
+        <Col>
+          <Card>
+            <Card.Header>
+              <h4>Transaction History</h4>
+            </Card.Header>
+            <Card.Body>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Date</th>
+                    <th>Total Amount</th>
+                    <th>Employee ID</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {transactions.map((transaction) => (
+                    <tr key={transaction.id}>
+                      <td>{transaction.id}</td>
+                      <td>{transaction.date}</td>
+                      <td>${transaction.totalAmount}</td>
+                      <td>{transaction.employeeId}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Transaction;
+export default TransactionHistory;
